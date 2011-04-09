fa63c5c7fbca488ddbed417382fc05c1f947f37f
Mark Steward <marksteward@gmail.com>
2011-04-10 00:40:19 +0100

    Simple schedule management

--- commands.py
+++ commands.py
@@ -2,23 +2,23 @@ import chanbot
 import time
 from datetime import datetime, timedelta
 from urllib import urlopen
-from irclib import nm_to_n 
+from decorator import decorator
+from irclib import nm_to_n, is_channel
 
 STATS_URL = 'http://london.hackspace.org.uk/members/member_stats.php'
 OPS_CHAN = '#london-hack-space'
-CHANSERV = 'chanserv'
+#CHANSERV = 'chanserv'
 CHANSERV = '#london-hack-space-dev'
 TRUSTED_NICK = 'ms7821'
 
 # Wrap callbacks so we use the latest code
 
-def callback(f):
-    def wrapped(*args, **kwargs):
-        try:
-            f(*args, **kwargs)
-        except Exception, e:
-            print 'Exception %s during callback' % repr(e)
-    return wrapped
+@decorator
+def callback(f, *args, **kwargs):
+    try:
+        f(*args, **kwargs)
+    except Exception, e:
+        print 'Exception %s during callback' % repr(e)
 
 @callback
 def update_subs(*args, **kwargs):
@@ -36,17 +36,33 @@ def _handle_command(bot, e, cmd, args):
 
     c = bot.connection
     nick = nm_to_n(e.source())
+    if is_channel(e.target()):
+        target = e.target()
+    else:
+        target = nick
 
     if cmd in ['die', 'quit', 'foad']:
         bot.die(msg='Quit requested by %s' % nick)
 
     elif cmd == 'ping':
-        if e.target().startswith('#'):
-            t = e.target()
-        else:
-            t = nick
+        c.privmsg(target, 'Pong!')
 
-        c.privmsg(t, 'Pong!')
+    elif cmd == 'sched':
+        for t, f, a in c.irclibobj.delayed_commands:
+            def argstr(a):
+                if repr(a) == repr(bot):
+                    return 'bot'
+                return repr(a)
+
+            c.privmsg(target, '%s %s(%s)' % (
+                time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(t)),
+                f.__name__,
+                ', '.join(map(argstr, a))
+            ))
+
+    elif cmd == 'clearsched' and nick == TRUSTED_NICK:
+        c.irclibobj.delayed_commands = []
+        c.privmsg(target, 'Schedule cleared')
 
     elif cmd == 'subs' and nick == TRUSTED_NICK:
         at = time.strptime(args, '%H:%M')
@@ -63,7 +79,7 @@ def _handle_command(bot, e, cmd, args):
             c.privmsg(CHANSERV, 'deop %s %s' % (OPS_CHAN, args))
 
     else:
-        c.notice(nick, "I didn't understand \"%s\"" % cmd)
+        c.notice(target, "I didn't understand \"%s\"" % cmd)
 
 def _schedule_update_subs(bot, hour, minute):
     now = datetime.now()
